558e67f19fe908e4eb013ba74e1e76b78c06f3e7
AleksGlinskiy <lglinskii67@gmail.com>
2024-03-07 01:12:07 +0300

    fix Skeleton

--- src/shared/ui/Skeleton/Skeleton.tsx
+++ src/shared/ui/Skeleton/Skeleton.tsx
@@ -3,11 +3,15 @@ import cls from './Skeleton.module.scss';
 
 interface SkeletonProps {
     className?: string;
+    width?: number;
+    height?: number;
 }
 
 export function Skeleton(props: SkeletonProps) {
     const {
         className,
+        width,
+        height,
     } = props;
 
     return (
