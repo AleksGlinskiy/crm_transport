39b7897803da80262a4ae371b098ef98497fb86a
AleksGlinskiy <lglinskii67@gmail.com>
2024-03-10 20:39:40 +0300

    fix svrg loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -46,7 +46,22 @@ export function buildLoaders({ isDev, paths }: BuildOptions): webpack.RuleSetRul
     const svgLoader = {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack'],
+        use: [{
+            loader: '@svgr/webpack',
+            options: {
+                icon: true,
+                svgoConfig: {
+                    plugins: [
+                        {
+                            name: 'convertColors',
+                            params: {
+                                currentColor: true,
+                            },
+                        },
+                    ],
+                },
+            },
+        }],
     };
 
     const fontLoader = {
