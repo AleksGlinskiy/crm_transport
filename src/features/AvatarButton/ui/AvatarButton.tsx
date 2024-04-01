59994eaa1138d363594a8443a56bd84c6411e02a
AleksGlinskiy <lglinskii67@gmail.com>
2024-04-02 00:42:00 +0300

    fix AvatarButton

--- src/features/AvatarButton/ui/AvatarButton.tsx
+++ src/features/AvatarButton/ui/AvatarButton.tsx
@@ -1,8 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
-import { Button } from '@/shared/ui/Button/Button';
 import { getUserAuthData, userActions } from '@/entities/User';
-import IconInfo from '@/shared/assets/icons/info-circle.svg';
+import IconLogin from '@/shared/assets/icons/login.svg';
 import IconUser from '@/shared/assets/icons/user.svg';
 import cls from './AvatarButton.module.scss';
 import { AppImage } from '@/shared/ui/AppImage/AppImage';
@@ -41,16 +40,17 @@ export function AvatarButton(props: AvatarButtonProps) {
                 fallback={fallback}
                 errorFallback={errorFallback}
                 className={cls.AvatarButton__avatar}
-                src="https://sea2.discourse-cdn.com/business7/user_avatar/forum.codewithmosh.com/adomovic/48/5754_2.png"
+                src={authData.avatar}
                 alt={authData.username}
             />
             <div className={cls.AvatarButton__text}>
                 <div className={cls.AvatarButton__name}>{authData.username}</div>
                 <div className={cls.AvatarButton__role}>admin</div>
             </div>
-            <Button className={cls.AvatarButton__btn} onClick={() => handleLogout()}>
-                <IconInfo />
-            </Button>
+
+            <button type="button" aria-label="logout" className={cls.AvatarButton__btn} onClick={() => handleLogout()}>
+                <IconLogin />
+            </button>
         </div>
     );
 }
