28a8c7375e2ed1aca56f716dd33e0206945b4c47
AleksGlinskiy <lglinskii67@gmail.com>
2024-04-05 17:04:59 +0300

    fix constants

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { USER_AUTH_DATA } from '@/shared/const/const';
 
 export const $api = axios.create();
 $api.interceptors.request.use((config) => {
     config.baseURL = 'http://localhost:8000/';
-    config.headers.authorization = localStorage.getItem('user') || '';
+    config.headers.authorization = localStorage.getItem(USER_AUTH_DATA) || '';
 
     return config;
 });
