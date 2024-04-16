03d6ac6a6b5de9c642de47653059cc27da6c4c50
AleksGlinskiy <lglinskii67@gmail.com>
2024-04-16 03:00:58 +0300

    add YMaps in stopoverDetailsPage

--- src/pages/StopoverDetailsPage/ui/StopoverDetailsPage.tsx
+++ src/pages/StopoverDetailsPage/ui/StopoverDetailsPage.tsx
@@ -1,13 +1,24 @@
+import { useParams } from 'react-router-dom';
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 import useTitle from '@/shared/hooks/useTitle';
 import { PageHeader } from '@/widgets/PageHeader';
 
 export default function StopoverDetailsPage() {
     useTitle('Дворец культуры железнодорожников - Остановочный пункт');
 
+    const { id } = useParams<{ id: string }>();
+
     return (
         <>
             <PageHeader title='Дворец культуры железнодорожников' />
-            text
+            <YMaps>
+                <Map
+                    defaultState={{ center: [55.75, 37.57], zoom: 9 }}
+                    style={{ width: '100%', height: '600px' }}
+                >
+                    <Placemark defaultGeometry={[55.751574, 37.573856]} />
+                </Map>
+            </YMaps>
         </>
     );
 }
